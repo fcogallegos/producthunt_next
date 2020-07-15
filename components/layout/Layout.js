0792956569cb72a0fbf8b3061e2947a07638247c
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-15 16:30:31 -0600

    adding fonts of google fonts and more

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import { Global, css } from '@emotion/core';
+import Head from 'next/head';
 
 const Layout = props => {
     return ( 
@@ -23,10 +24,18 @@ const Layout = props => {
                     body {
                         font-size: 1.6rem;
                         line-height: 1.5;
+                        font-family: 'PT Sans', sans-serif;
                     }
                     h1, h2, h3 {
                         margin: 0 0 2rem 0;
                         line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto Slab', serif;
+                        font-weight: 700;
+                    }
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
                     }
 
                     ul {
@@ -39,6 +48,14 @@ const Layout = props => {
                     }
                 `}
             />
+
+            <Head>
+                    <title>Product Hunt Firebase and Next.js</title>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+                    <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet"/>
+                    <link href="/static/css/app.css" rel="stylesheet" />
+            </Head>    
+
             <Header />
 
             <main>
