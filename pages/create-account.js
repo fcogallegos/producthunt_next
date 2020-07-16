3d655992bb5c0a22afaa4e91dc49d746e2205b8c
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-16 15:11:27 -0600

    creating the form for create accounts

--- pages/create-account.js
+++ pages/create-account.js
@@ -6,7 +6,45 @@ const CreateAccount = () => (
   
     <div>
       <Layout>
-        <h1>Create Account</h1>
+        <>
+          <h1>Create Account</h1>
+          <form>
+              <div>
+                  <label htmlFor="name">Name</label>
+                  <input 
+                      type="text"
+                      id="name"
+                      placeholder="Your name"
+                      name="name"
+                  />
+              </div>
+
+              <div>
+                  <label htmlFor="email">Email</label>
+                  <input 
+                      type="email"
+                      id="email"
+                      placeholder="Your Email"
+                      name="email"
+                  />
+              </div>
+
+              <div>
+                  <label htmlFor="password">Password</label>
+                  <input 
+                      type="password"
+                      id="password"
+                      placeholder="Your password"
+                      name="password"
+                  />
+              </div>
+
+              <input 
+                  type="submit"
+                  value="Create Account"
+              />
+          </form>
+        </>
       </Layout>
     </div>
   
