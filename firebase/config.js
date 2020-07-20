be6492dd21ad6c8e5595d57f06de20864aedd032
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-20 10:09:04 -0600

    creating the configuration files of firebase

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCuVwPq9HTUVamkxiph2OrsuWV41bWvThg",
+    authDomain: "product-hunt-5129e.firebaseapp.com",
+    databaseURL: "https://product-hunt-5129e.firebaseio.com",
+    projectId: "product-hunt-5129e",
+    storageBucket: "product-hunt-5129e.appspot.com",
+    messagingSenderId: "410476947789",
+    appId: "1:410476947789:web:af96c764cd5d26793bef04"
+  };
+
+  export default firebaseConfig;
