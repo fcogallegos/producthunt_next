628aedbf557249da5f37624b521da28bf60218f8
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-22 12:29:05 -0600

    consulting to database

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import styled from '@emotion/styled';
 import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
 
 const Heading = styled.h1`
   color: blue;
 `;
 
-const Home = () => (
+const Home = () => {
+
+  const [ products, saveProducts ] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+  
+  useEffect(() => {
+    const getProducts = () => {
+      firebase.db.collection('products').orderBy('created', 'desc').onSnapshot(handleSnapshot)
+    }
+    getProducts();
+  }, [])
+
+  function handleSnapshot(snapshot) {
+    const products = snapshot.docs.map( doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+
+    saveProducts(products);
+  }
+
+  return (
   
     <div>
       <Layout>
@@ -15,5 +40,6 @@ const Home = () => (
     </div>
   
 )
+}
 
 export default Home;
