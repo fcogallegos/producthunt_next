0d8c8e92af5c37a24625b549ea4152e9f33f3c20
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-22 15:14:24 -0600

    adding CSS to product list

--- components/layout/DetailsProduct.js
+++ components/layout/DetailsProduct.js
@@ -2,38 +2,116 @@ import React from 'react';
 import styled from '@emotion/styled';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
+
+const Product = styled.li`
+    padding: 4rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #e1e1e1;
+`;
+
+const DescriptionProduct = styled.div`
+    flex: 0 1 600px;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    column-gap: 2rem;
+`;
+
+const Title = styled.a`
+    font-size: 2rem;
+    font-weight: bold;
+    margin: 0;
+
+    :hover {
+        cursor: pointer;
+    }
+`;
+
+const TextDescription = styled.p`
+    font-size: 1.6rem;
+    margin: 0;
+    color: #888;
+`;
+
+const Comments = styled.div`
+    margin-top: 2rem;
+    display: flex;
+    align-items: center;
+    div {
+        display: flex;
+        align-items: center;
+        border: 1px solid #e1e1e1;
+        padding: .3rem 1rem;
+        margin-right: 2rem;
+    }
+    img {
+        width: 2rem;
+        margin-right: 2rem;
+    }
+    p {
+        font-size: 1.6rem;
+        margin-right: 1rem;
+        font-weight: 700;
+        &:last-of-type {
+            margin: 0;
+        }
+    }
+`;
+
+const Image = styled.img`
+    width: 200px;
+`;
+
+const Votes = styled.div`
+    flex: 0 0 auto;
+    text-align: center;
+    border: 1px solid #e1e1e1;
+    padding: 1rem 3rem;
+
+    div {
+        font-size: 2rem;
+    }
+
+    p {
+        margin: 0;
+        font-size: 2rem;
+        font-weight: 700;
+    }
+`;
+
 const DetailsProduct = ({product}) => {
     
     const { id, comments, created, description, company, name, url, urlimage, votes } = product;
     
 
     return ( 
-        <li>
-            <div>
+        <Product>
+            <DescriptionProduct>
                 <div>
-                    <img src={urlimage} />
+                    <Image src={urlimage} />
                 </div>
                 <div>
-                    <h1>{name}</h1>
+                    <Title>{name}</Title>
 
-                    <p>{description}</p>
+                    <TextDescription>{description}</TextDescription>
 
-                    <div>
-                        <img src="/static/img/comentario.png" />
-                        <p>{comments.length} Comments</p>
+                    <Comments>
+                        <div>
+                            <img src="/static/img/comentario.png" />
+                            <p>{comments.length} Comments</p>
+                        </div>                       
+                    </Comments>
 
-                        <p>Posted { formatDistanceToNow(new Date(created))} ago </p>
-                    </div>
+                    <p>Posted { formatDistanceToNow(new Date(created))} ago </p>
                 </div>
-            </div>
+            </DescriptionProduct>
 
-            <div>
+            <Votes>
                 <div> &#9650; </div>
                 <p>{votes}</p>
-            </div>
-        </li>
-
-        
+            </Votes>
+        </Product>
      );
 }
  
