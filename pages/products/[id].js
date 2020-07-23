b6fc258c7a0dbd653678dfc433b969dd9e9b28d4
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-23 14:22:18 -0600

    adding the margin to the products

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -4,6 +4,22 @@ import { useRouter } from 'next/router';
 import Layout from '../../components/layout/Layout';
 import { FirebaseContext } from '../../firebase';
 import Error404 from '../../components/layout/404';
+import styled from '@emotion/styled';
+
+
+const H1 = styled.h1`
+    text-align: center;
+    margin-top: 5rem;
+`;
+
+const ContainerProduct = styled.div`
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        column-gap: 2rem;
+    }
+   
+`;
 
 const Product = () => {
 
@@ -33,9 +49,31 @@ const Product = () => {
         }
     }, [id]);
 
+    if(Object.keys(product).length === 0) return 'Loading...';
+
+    const { comments, created, description, company, name, url, urlimage, votes } = product;
+    
+
     return ( 
         <Layout>
-            { error && <Error404 /> }
+            <>
+                { error && <Error404 /> }
+
+                <div className="container">
+                    <H1>{name}</H1>
+                
+
+                    <ContainerProduct>
+                        <div>
+                            1
+                        </div>
+        
+                        <aside>
+                            2
+                        </aside>
+                    </ContainerProduct>
+                </div>
+            </>
         </Layout> 
     );
 }
