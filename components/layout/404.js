fe4f096b7beb22e8c308f627c2151b8246a236fa
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-23 13:13:41 -0600

    validation if a product exist

---
+++ components/layout/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Title = styled.h1`
+    margin-top: 5rem;
+    text-align: center;
+`;
+
+const Error404 = () => {
+    return ( <Title>Not existent Product</Title> );
+}
+ 
+export default Error404;
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,18 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { useRouter } from 'next/router';
 
+import Layout from '../../components/layout/Layout';
+import { FirebaseContext } from '../../firebase';
+import Error404 from '../../components/layout/404';
+
 const Product = () => {
+
+    //state of the component
+    const [ product, saveProduct ] = useState({});
+    const [ error, saveError ] = useState(false);
+
     //Routing for get the current "id"
     const router = useRouter();
     const { query: { id } } = router;
 
+    //context of firebase
+    const { firebase } = useContext(FirebaseContext);
+
     useEffect(() => {
         if(id) {
-            console.log("There is an id ", id);
+            const getProduct = async () => {
+                const productQuery = await firebase.db.collection('products').doc(id);
+                const product = await productQuery.get();
+                if(product.exists) {
+                    saveProduct( product.data() );
+                } else {
+                    saveError( true );
+                }
+            }
+            getProduct();
         }
-    }, [id])
+    }, [id]);
 
-    return ( <h1>From {id}</h1> );
+    return ( 
+        <Layout>
+            { error && <Error404 /> }
+        </Layout> 
+    );
 }
  
 export default Product;
